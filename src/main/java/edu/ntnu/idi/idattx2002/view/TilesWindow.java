08c5b6f12b5934179f255bc02051322109de0129
kasper280403 <kasper.s.karlsen@gmail.com>
2025-02-13 13:45:50 +0100

    Reset

---
+++ src/main/java/edu/ntnu/idi/idattx2002/view/TilesWindow.java
@@ -0,0 +1,20 @@
+package edu.ntnu.idi.idattx2002.view;
+import javafx.scene.layout.VBox;
+
+
+public class TilesWindow{
+
+
+    public static VBox getBoard(int xDimensions, int yDimensions){
+        VBox box = new VBox();
+        for (int i = 0; i < xDimensions; i++) {
+            Vbox
+            for (int j = 0; j < yDimensions; j++) {
+
+            }
+        }
+
+        return box;
+    }
+
+}
