5684fe9f8c503c9390563515bf22884593d509cf
Sindre <Sindrelmjoes@gmail.com>
2025-02-13 15:30:39 +0100

    Updated LadderActionTest

--- src/test/java/LadderActionTest.java
+++ src/test/java/LadderActionTest.java
@@ -3,24 +3,24 @@ import static org.junit.Assert.assertEquals;
 import edu.ntnu.idi.idattx2002.Modules.Board.Actions.LadderAction;
 import edu.ntnu.idi.idattx2002.Modules.Player.Player;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LadderActionTest {
-  Player player;
-
-  @BeforeEach
-  void setUp() {
-    player = new Player("Kasper", 1, 1);
-  }
 
   @Test
   void testPerform() {
     //Arrange
     LadderAction ladderTo10 = new LadderAction(10);
+    Player player = new Player("Kasper", 1, 1);
     //act
     ladderTo10.perform(player);
     //Assert
     Assertions.assertEquals(10, player.getCurrentTile());
+  }
+
+  @Test
+  void testInvalidDestinationTileId() {
+    //Assert
+    Assertions.assertThrows(IllegalArgumentException.class, () -> new LadderAction(-1));
   }
 }
