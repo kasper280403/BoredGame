81b866d72e71a83b8092a24272e2434af978bca3
Sindre <Sindrelmjoes@gmail.com>
2025-02-14 11:44:53 +0100

    Created constructor and addTile method to Board

--- src/main/java/edu/ntnu/idi/idattx2002/Modules/Board/Board.java
+++ src/main/java/edu/ntnu/idi/idattx2002/Modules/Board/Board.java
@@ -1,4 +1,17 @@
 package edu.ntnu.idi.idattx2002.Modules.Board;
+import edu.ntnu.idi.idattx2002.Modules.Board.Tile;
+import java.util.HashMap;
+
 
 public class Board {
+
+  private HashMap<Integer, Tile> tiles;
+
+  public Board() {
+    this.tiles = new HashMap<>();
+  }
+
+  public void addTile(Tile tile) {
+    tiles.put(tile.getTileId(), tile);
+  }
 }
