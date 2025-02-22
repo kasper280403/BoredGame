639e42d6e748a6d8467c7f0bd40042cb0b0a904b
Sindre <Sindrelmjoes@gmail.com>
2025-02-22 21:44:35 +0100

    Created static Random object in DiceView

--- src/main/java/edu/ntnu/idi/idattx2002/view/DiceWindow.java
+++ src/main/java/edu/ntnu/idi/idattx2002/view/DiceWindow.java
@@ -14,6 +14,7 @@ import java.util.Random;
 
 public class DiceWindow {
 
+    private static Random random = new Random();
     private static final GridPane dicePane = new GridPane();
     private static final HashMap<String, ImageView> diceMap = new HashMap<>();
     static HashMap<Integer, Image> greenDiceImageMap = new HashMap<>();
@@ -67,7 +68,6 @@ public class DiceWindow {
 
     public static void throwDice(int A, int B){
 
-        Random random  = new Random();
         Timeline timeline = new Timeline();
 
         for (int i = 0; i < 13; i++) {
