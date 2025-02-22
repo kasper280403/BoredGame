9f979e58731bd49fa9a42ed8befff8b92286bd96
Sindre <Sindrelmjoes@gmail.com>
2025-02-22 13:00:03 +0100

    Added method for displaying gamePieces in GameWindow, and refactored getBoard method

--- src/main/java/edu/ntnu/idi/idattx2002/GameWindow.java
+++ src/main/java/edu/ntnu/idi/idattx2002/GameWindow.java
@@ -21,6 +21,9 @@ public class GameWindow extends Application {
         GridPane board = TilesWindow.getBoard(9, 10);
         GridPane dice = DiceWindow.getDice();
 
+        TilesWindow.displayPieceAtTile(1, 1);
+        TilesWindow.displayPieceAtTile(2, 2);
+
         Button colorChanger = new Button("Change Color");
         colorChanger.setOnAction(e -> changeColor(3));
 
--- src/main/java/edu/ntnu/idi/idattx2002/view/TilesWindow.java
+++ src/main/java/edu/ntnu/idi/idattx2002/view/TilesWindow.java
@@ -1,4 +1,7 @@
 package edu.ntnu.idi.idattx2002.view;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -8,17 +11,18 @@ import javafx.scene.text.Text;
 import java.util.HashMap;
 
 
-public class TilesWindow{
-    private static final HashMap<Integer, Rectangle> tileMap = new HashMap<>();
+public class TilesWindow {
 
-    public static GridPane getBoard(int xDimensions, int yDimensions){
+    private static final HashMap<Integer, StackPane> tileMap = new HashMap<>();
+
+    public static GridPane getBoard(int xDimensions, int yDimensions) {
 
         GridPane gridPane = new GridPane();
         double tileSize = 50;
         int tileID = 1;
         boolean leftToRight = true;
 
-        for (int row = xDimensions-1; row >= 0; row--) {
+        for (int row = xDimensions - 1; row >= 0; row--) {
 
             if (!leftToRight) {
                 tileID += yDimensions - 1;
@@ -27,18 +31,18 @@ public class TilesWindow{
             for (int col = 0; col < yDimensions; col++) {
                 Rectangle tile = new Rectangle(tileSize, tileSize);
 
-                tileMap.put(tileID, tile);
-
                 if ((row + col) % 2 == 0) {
                     tile.setFill(Color.LIGHTGRAY);
                 } else {
                     tile.setFill(Color.DARKGRAY);
                 }
-
                 tile.setStroke(Color.BLACK);
-                Text tileText = new Text(""+tileID);
+
+                Text tileText = new Text("" + tileID);
                 StackPane tilePane = new StackPane();
                 tilePane.getChildren().addAll(tile, tileText);
+
+                tileMap.put(tileID, tilePane);
 
                 gridPane.add(tilePane, col, row);
 
@@ -57,11 +61,34 @@ public class TilesWindow{
     }
 
 
+    //TODO refactor!!
+    public static void displayPieceAtTile(int tileID, int pieceID) {
+        StackPane tilePane = tileMap.get(tileID);
+        if (pieceID == 1) {
+            Image pieceImage = new Image("/images/pieces/frogPiece.png");
+            ImageView pieceView = new ImageView(pieceImage);
+            pieceView.setFitWidth(60);
+            pieceView.setFitHeight(60);
+            tilePane.getChildren().add(pieceView);
+        } else if (pieceID == 2) {
+            Image pieceImage = new Image("/images/pieces/catPiece.png");
+            ImageView pieceView = new ImageView(pieceImage);
+            pieceView.setFitWidth(60);
+            pieceView.setFitHeight(60);
+            tilePane.getChildren().add(pieceView);
+        }
+
+    }
+
     public static void changeTileColor(int tileID, Color color) {
-        Rectangle tile = tileMap.get(tileID);
-        if (tile != null) {
-            tile.setFill(color);
+        StackPane tilePane = tileMap.get(tileID);
+        if (tilePane != null) {
+            for (Node node : tilePane.getChildren()) {
+                if (node instanceof Rectangle tile) {
+                    tile.setFill(color);
+                    break;
+                }
+            }
         }
     }
 }
-
