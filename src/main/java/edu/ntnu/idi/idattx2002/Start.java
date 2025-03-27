104c8bfe0e4a8dec93a496672eaf08493065d8b9
kasper280403 <kasper.s.karlsen@gmail.com>
2025-03-27 13:47:02 +0100

    Choosing players for game now works

--- src/main/java/edu/ntnu/idi/idattx2002/Start.java
+++ src/main/java/edu/ntnu/idi/idattx2002/Start.java
@@ -1,5 +1,6 @@
 package edu.ntnu.idi.idattx2002;
 
+import edu.ntnu.idi.idattx2002.view.ChoosePlayerWindow;
 import edu.ntnu.idi.idattx2002.view.PieceWindow;
 import edu.ntnu.idi.idattx2002.view.CreatePlayerWindow;
 import edu.ntnu.idi.idattx2002.view.SnakesAndLadderWindow;
@@ -7,10 +8,7 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.util.ArrayList;
@@ -30,92 +28,47 @@ public class Start extends Application {
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
-        primaryStage.setTitle("Chose game");
+        primaryStage.setTitle("Choose game");
 
-        Label label = new Label("Chose game:");
-        Button snakesAndLaddersButton = new Button("Snakes and Ladders");
-        snakesAndLaddersButton.setOnAction(e -> openPlayerInput());
+        Label label = new Label("Choose game:");
+        Button chooseGameButton = new Button("Choose Game");
+        chooseGameButton.setOnAction(e -> openGamesWindow(primaryStage));
 
         Button createPlayerButton = new Button("Create Player");
         createPlayerButton.setOnAction(e -> createPlayerWindow.openPlayerInput());
 
         VBox layout = new VBox(10);
         layout.setAlignment(Pos.CENTER);
-        layout.getChildren().addAll(label, createPlayerButton,  snakesAndLaddersButton);
+        layout.getChildren().addAll(label, createPlayerButton,  chooseGameButton);
 
         Scene scene = new Scene(layout, 300, 200);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    //Må refaktoreres litt her, denne kan være generell for flere spill men kan ikke kalle på startsnakeAndladders herifra
-    private void openPlayerInput() {
+
+    private void openGamesWindow(Stage primaryStage) {
         VBox layout = new VBox(10);
-        Label heading = new Label("Add players!");
-        Region spacing = new Region();
-        spacing.setMinHeight(20);
-
-        Label playerNameLabel = new Label("Player name:");
-        TextField playerNameField = new TextField();
-        HBox playerNameHBox = new HBox(20);
-        playerNameHBox.setAlignment(Pos.CENTER);
-        playerNameHBox.getChildren().addAll(playerNameLabel, playerNameField);
+        Label heading = new Label("Choose Game");
+        ChoosePlayerWindow choosePlayerWindow = new ChoosePlayerWindow(primaryStage);
 
 
-        HBox pieces = new HBox(10);
-        ListView<String> playerListView = new ListView<>();
-        HashMap<Integer, ImageView> images = getPiecesImg();
-        HashMap<Integer, Button> pieceButtons = new HashMap<>();
+        Button snakesAndLadders = new Button("Snakes and ladders");
 
-        final int[] selectedPiece = {1};
 
-        for (Integer key : images.keySet()) {
-            VBox pieceBox = new VBox(10);
-            Label imgTxt = new Label("Piece " + key);
-            Button selectPieceButton = new Button("Pick me");
 
-            selectPieceButton.setOnAction(e -> {
-                selectedPiece[0] = key;
-                pieceButtons.values().forEach(b -> b.setDisable(false));
-                selectPieceButton.setDisable(true);
-            });
-
-            pieceBox.getChildren().addAll(images.get(key), imgTxt, selectPieceButton);
-            pieces.getChildren().add(pieceBox);
-            pieceButtons.put(key, selectPieceButton);
-        }
-
-        pieces.setAlignment(Pos.CENTER);
-
-        Button addPlayerButton = new Button("Add player");
-        Button startGameButton = new Button("Start game");
-        startGameButton.setDisable(true);
-
-        addPlayerButton.setOnAction(e -> {
-            String name = playerNameField.getText().trim();
-            if (!name.isEmpty() && !playerNames.contains(name)) {
-                playerNames.add(name);
-                playerPieces.add(selectedPiece[0]);
-                playerListView.getItems().add(name + " (Piece " + selectedPiece[0] + ")");
-                playerNameField.clear();
-            }
-            if (playerNames.size() >= 2) {
-                startGameButton.setDisable(false);
-            }
+        snakesAndLadders.setOnAction(e -> {
+            choosePlayerWindow.selectPlayers(2);
+            //new SnakesAndLadderWindow(primaryStage, playerNames, playerPieces);
         });
 
-        startGameButton.setOnAction(e -> startSnakesAndLadders());
-
-        layout.getChildren().addAll(heading, playerNameHBox, pieces, spacing, addPlayerButton, playerListView, startGameButton);
+        layout.getChildren().addAll(heading, snakesAndLadders);
         layout.setAlignment(Pos.CENTER);
         layout.setSpacing(10);
         Scene scene = new Scene(layout, 500, 400);
         primaryStage.setScene(scene);
     }
 
-    private void startSnakesAndLadders() {
-        new SnakesAndLadderWindow(primaryStage, playerNames, playerPieces);
-    }
 
     public HashMap<Integer, ImageView> getPiecesImg() {
         HashMap<Integer, ImageView> imageViewMap = new HashMap<>();
