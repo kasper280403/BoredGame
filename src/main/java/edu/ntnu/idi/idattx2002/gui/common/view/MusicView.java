06438d45933fa112ec070aac7c6b2c5554b9fa11
Sindre <Sindrelmjoes@gmail.com>
2025-05-04 18:33:03 +0200

    Added music info to music view

--- src/main/java/edu/ntnu/idi/idattx2002/gui/common/view/MusicView.java
+++ src/main/java/edu/ntnu/idi/idattx2002/gui/common/view/MusicView.java
@@ -1,6 +1,7 @@
 package edu.ntnu.idi.idattx2002.gui.common.view;
 
 import edu.ntnu.idi.idattx2002.gui.common.controller.MusicController;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Background;
@@ -12,17 +13,46 @@ import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public class MusicView extends VBox {
 
   private MusicController controller;
   private Pane mainPane;
 
+  private Text currentlyPlayingText;
+
   public MusicView(MusicController controller, Pane mainPane) {
     this.controller = controller;
     this.mainPane = mainPane;
 
     init();
+  }
+
+  private Text createTopInfoText() {
+    Text topInfo = new Text("Currently Playing:");
+    topInfo.setFont(new Font("Helvetica", 12));
+    topInfo.setFill(Color.LIGHTBLUE);
+
+    return topInfo;
+  }
+
+  private void createCurrentlyPlayingText() {
+    currentlyPlayingText = new Text("Nothing :/");
+    currentlyPlayingText.setFont(new Font("Helvetica", 18));
+    currentlyPlayingText.setFill(Color.LIGHTBLUE);
+  }
+
+  private VBox createMusicInfoPane() {
+    VBox musicInfoPane = new VBox();
+    musicInfoPane.setAlignment(Pos.CENTER);
+    musicInfoPane.setSpacing(3);
+
+    createCurrentlyPlayingText();
+
+    musicInfoPane.getChildren().addAll(createTopInfoText(), currentlyPlayingText);
+    return musicInfoPane;
   }
 
   private Button createPlayBtn() {
@@ -58,12 +88,13 @@ public class MusicView extends VBox {
   private void init() {
     setAlignment(Pos.CENTER);
     setSpacing(5);
+    setPadding(new Insets(15));
     setBackground(new Background(new BackgroundFill(Color.GREY, new CornerRadii(5), null)));
     setMaxSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
 
     StackPane.setAlignment(this, Pos.BOTTOM_LEFT);
 
-    getChildren().add(createBtnPane());
+    getChildren().addAll(createMusicInfoPane(), createBtnPane());
   }
 
   public void show() {
