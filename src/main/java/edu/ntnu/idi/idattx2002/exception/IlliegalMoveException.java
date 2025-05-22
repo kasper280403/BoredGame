b246247745491d410294f9256496d869762d1b07
Sindre <Sindrelmjoes@gmail.com>
2025-05-22 10:38:32 +0200

    created illiegalMoveException

---
+++ src/main/java/edu/ntnu/idi/idattx2002/exception/IlliegalMoveException.java
@@ -0,0 +1,8 @@
+package edu.ntnu.idi.idattx2002.exception;
+
+public class IlliegalMoveException extends GameException{
+
+  public IlliegalMoveException(String message) {
+    super(message);
+  }
+}
